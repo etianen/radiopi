25ed997ea43dfa144378dd8163efdf8c0e4fcc3b
Dave Hall <dave@etianen.com>
2024-01-08 22:50:38 +0000

    wip

--- radio.py
+++ radio.py
@@ -1,77 +1,12 @@
 from __future__ import annotations
 
-import subprocess
 from signal import pause
 
 from gpiozero import Button, Device  # type: ignore[import-untyped]
 from gpiozero.pins.rpigpio import RPiGPIOFactory  # type: ignore[import-untyped]
-from rich.console import Console
 
-from stations import Station, load_stations
-
-RADIO_CLI = "/opt/DABBoard/radio_cli_v3.1.0"
-
-
-class Radio:
-    def __init__(self) -> None:
-        self.console = Console()
-        self.is_playing = False
-        # Load all stations.
-        self.stations: list[Station] = load_stations()
-        self.station_index: int = 0
-        # Start the radio.
-        self.console.print("[green bold]Hello RadioPI![/]")
-        self.toggle_play()
-
-    def cli(self, *args: str) -> None:
-        subprocess.check_call((RADIO_CLI, *args))
-
-    def play(self) -> None:
-        # Boot, if not playing.
-        if not self.is_playing:
-            self.console.print("[cyan]Booting radio...[/]")
-            self.cli("--boot=D")
-            self.is_playing = True
-            self.console.print("[green]Radio booted![/]")
-        # Tune to the station.
-        station: Station = self.stations[self.station_index % len(self.stations)]
-        self.console.print(f"Tuning to [cyan]{station.label}[/]...")
-        self.console.print(station)
-        self.cli(
-            f"--component={station.component_id}",
-            f"--service={station.service_id}",
-            f"--frequency={station.frequency_index}",
-            "--play",
-            "--level=63",
-        )
-        self.console.print(f"Playing [green]{station.label}[/]!")
-
-    def stop(self) -> None:
-        # Shutdown, if playing.
-        if self.is_playing:
-            self.console.print("[cyan]Shutting down radio...[/]")
-            self.cli("--shutdown")
-            self.is_playing = False
-            self.console.print("[red]Radio shutdown![/]")
-
-    def toggle_play(self) -> None:
-        if self.is_playing:
-            self.stop()
-        else:
-            self.play()
-
-    def next_station(self) -> None:
-        self.station_index += 1
-        self.play()
-
-    def prev_station(self) -> None:
-        self.station_index -= 1
-        self.play()
-
-    def shutdown(self) -> None:
-        self.stop()
-        self.console.print("[cyan bold]Goodby RadioPi...[/]")
-        subprocess.check_call(["poweroff", "-h"])
+from radiopi.radio import Radio
+from radiopi.stations import Station, load_stations
 
 
 def main():
