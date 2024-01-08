beaea5529935d55f9cd2d55b07b14bef845cada3
Dave Hall <dave@etianen.com>
2024-01-08 20:09:35 +0000

    wip

--- radiopi/__init__.py
+++ radiopi/__init__.py
@@ -2,10 +2,21 @@ from __future__ import annotations
 
 from argparse import ArgumentParser
 
+from radiopi import run
+
 
 def main() -> None:
+    # Create parser.
     parser = ArgumentParser(
-        description="All we hear is RadioPi...",
         allow_abbrev=False,
+        description="All we hear is RadioPi...",
     )
-    args = parser.parse_args()
+    # Create subparsers.
+    subparsers = parser.add_subparsers(metavar="COMMAND", required=True)
+    for cmd in (run.main,):
+        cmd_parser = subparsers.add_parser("run", description=cmd.__doc__, help=cmd.__doc__)
+        cmd_parser.set_defaults(cmd=cmd)
+    # Parse args.
+    args = vars(parser.parse_args())
+    cmd = args.pop("cmd")
+    cmd(**args)
---
+++ radiopi/run.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+
+def main() -> None:
+    """
+    Runs the RadioPi control daemon.
+    """
+    print("RUN")
