a7c8750bf8fd9c578a1c5b639617d22dcbbecab0
Dave Hall <dave@etianen.com>
2024-01-09 17:27:48 +0000

    wip

--- tests/test_pins.py
+++ tests/test_pins.py
@@ -5,5 +5,5 @@ from gpiozero.pins import Factory
 from radiopi.pins import PinFactory
 
 
-def test_pin_factory(pin_factory: PinFactory) -> None:
+def test_create_pin_factory(pin_factory: PinFactory) -> None:
     assert isinstance(pin_factory, Factory)
