593c2ca10e103f36a8eeaec0d244c1f71bfe79f5
Dave Hall <dave@etianen.com>
2024-01-09 11:13:38 +0000

    wip

--- radiopi/__init__.py
+++ radiopi/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import logging
 from signal import pause
 
 from radiopi.pins import discover_pin_factory
@@ -9,10 +10,11 @@ from radiopi.stations import load_stations
 
 
 def main() -> None:  # pragma: no cover
+    logging.basicConfig(format="[%(levelname)s] %(message)s", level=logging.INFO)
     pin_factory = discover_pin_factory()
     run = discover_run()
     stations = load_stations()
-    # Create the radio.
+    # Create radio.
     radio = Radio(
         pin_factory=pin_factory,
         run=run,
