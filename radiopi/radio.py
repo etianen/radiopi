45663923104ef7a03ab7130b5c4e7ce6f2054813
Dave Hall <dave@etianen.com>
2024-01-09 10:12:30 +0000

    wip

--- radiopi/radio.py
+++ radiopi/radio.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import dataclasses
+from threading import Condition
 
 from radiopi.subprocess import Run
 
@@ -14,3 +15,17 @@ class State:
 class Radio:
     def __init__(self, *, run: Run) -> None:
         self._run = run
+        self._state = State(is_playing=False, station_index=0)
+        self._condition = Condition()
+
+    @property
+    def state(self) -> State:
+        return self._state
+
+    @state.setter
+    def state(self, state: State) -> None:
+        with self._condition:
+            if state != self._state:
+                # Update the state and notify listeners.
+                self._state = state
+                self._condition.notify()
