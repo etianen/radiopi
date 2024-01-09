c0a35ed277e634fe10e193deb04b80337197371d
Dave Hall <dave@etianen.com>
2024-01-09 21:57:37 +0000

    wip

--- tests/test_running.py
+++ tests/test_running.py
@@ -6,8 +6,8 @@ from tests import QueueRunner
 
 def test_starts_and_pauses(runner: QueueRunner) -> None:
     with running(pin_factory_name="mock", runner=runner) as radio:
-        # On start the radio automatically boots and tunes.
+        # On start, the radio automatically boots and tunes.
         runner.assert_radio_booted()
         runner.assert_radio_tuned(radio.state.stations[0])
-    # On stop the radio automatically pauses.
+    # On stop, the radio automatically pauses.
     runner.assert_radio_paused()
