fd5bca6e86674a6c93ab244fd7b69de5c7c9302f
Dave Hall <dave@etianen.com>
2024-01-09 21:33:50 +0000

    wip

--- tests/test_radio.py
+++ tests/test_radio.py
@@ -4,8 +4,8 @@ from radiopi.radio import Radio
 
 
 def test_radio_state_initial(radio: Radio) -> None:
-    # The radio should be playing.
-    assert radio.state.playing
+    # The radio should not be playing.
+    assert not radio.state.playing
     # The stations should be loaded.
     assert radio.state.station_index == 0
     assert len(radio.state.stations) > 0
