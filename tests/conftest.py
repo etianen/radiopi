e19807f5e68497f2ac3e828362f9e704c7e57a6a
Dave Hall <dave@etianen.com>
2024-01-09 20:08:39 +0000

    wip

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,16 +12,3 @@ from radiopi.radio import Radio
 def radio() -> Generator[Radio, None, None]:
     with running(pin_factory_name="mock") as machine:
         yield machine
-
-
-# @pytest.fixture()
-# def radio(stations: Sequence[Station]) -> Generator[Radio, None, None]:
-#     # Create radio.
-#     radio = Radio(
-#         pin_factory=MockFactory(),
-#         run=run_mock,
-#         stations=stations,
-#     )
-#     # Run radio.
-#     with radio.running():
-#         yield radio
