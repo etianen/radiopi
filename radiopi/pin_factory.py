9f03f25fe8329289ed472f5ab5484e08b7ff9ded
Dave Hall <dave@etianen.com>
2024-01-09 21:22:24 +0000

    wip

--- radiopi/pin_factory.py
+++ radiopi/pin_factory.py
@@ -5,6 +5,8 @@ from contextlib import AbstractContextManager
 from pkgutil import resolve_name
 from typing import Literal, NewType
 
+from radiopi.log import log_contextmanager
+
 PinFactoryName = Literal["mock", "rpigpio"]
 PinFactory = NewType("PinFactory", object)
 
@@ -17,4 +19,4 @@ PIN_FACTORIES: Mapping[PinFactoryName, str] = {
 def create_pin_factory(pin_factory_name: PinFactoryName) -> AbstractContextManager[PinFactory]:
     pin_factory_path = PIN_FACTORIES[pin_factory_name]
     pin_factory_cls: type[AbstractContextManager[PinFactory]] = resolve_name(pin_factory_path)
-    return pin_factory_cls()
+    return log_contextmanager(name="Pin factory")(pin_factory_cls)()
