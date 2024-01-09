7eeb3d59159c680fbab19ac6a703f1e6ca8256c7
Dave Hall <dave@etianen.com>
2024-01-09 21:11:12 +0000

    wip

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,10 +6,10 @@ import pytest
 
 from radiopi import running
 from radiopi.radio import Radio
-from radiopi.runner import mock_runner
+from radiopi.runner import create_runner
 
 
 @pytest.fixture()
 def radio() -> Generator[Radio, None, None]:
-    with running(pin_factory_name="mock", runner=mock_runner) as machine:
+    with running(pin_factory_name="mock", runner=create_runner("mock")) as machine:
         yield machine
