5592a5ae5e61d2e662da3a62124b7e11eee465de
Dave Hall <dave@etianen.com>
2024-01-09 21:16:11 +0000

    wip

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from collections.abc import Mapping, Sequence
+from collections.abc import Sequence
 from queue import SimpleQueue
 
 from radiopi.runner import Runner
