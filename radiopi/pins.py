a94682b67bc14f0b98bbf199cbd759b629501c1b
Dave Hall <dave@etianen.com>
2024-01-09 19:25:55 +0000

    wip

--- radiopi/pins.py
+++ radiopi/pins.py
@@ -5,14 +5,15 @@ from pkgutil import resolve_name
 from typing import NewType
 
 PinFactory = NewType("PinFactory", object)
+PinFactoryFactory = NewType("PinFactoryFactory", str)
 
 
 PIN_FACTORIES = {
-    "mock": "gpiozero.pins.mock:MockFactory",
-    "rpigpio": "gpiozero.pins.rpigpio:RPiGPIOFactory",
+    "mock": PinFactoryFactory("gpiozero.pins.mock:MockFactory"),
+    "rpigpio": PinFactoryFactory("gpiozero.pins.rpigpio:RPiGPIOFactory"),
 }
 
 
-def create_pin_factory(name: str) -> AbstractContextManager[PinFactory]:
+def create_pin_factory(name: PinFactoryFactory) -> AbstractContextManager[PinFactory]:
     pin_factory_cls: type[AbstractContextManager[PinFactory]] = resolve_name(PIN_FACTORIES[name])
     return pin_factory_cls()
