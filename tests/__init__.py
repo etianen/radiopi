be723343adda325166f704db5c6543f0cc66f586
Dave Hall <dave@etianen.com>
2024-01-09 21:07:50 +0000

    wip

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,14 @@
 from __future__ import annotations
 
-from radiopi.runner import Runner
 from collections.abc import Mapping, Sequence
+from queue import SimpleQueue
+
+from radiopi.runner import Runner
 
 
 class QueueRunner(Runner):
+    def __init__(self) -> None:
+        self.queue: SimpleQueue[Sequence[str]] = SimpleQueue()
+
+    def _call(self, args: Sequence[str]) -> None:
+        self.queue.put_nowait(args)
