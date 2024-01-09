e1896057ad7c8ce1b8c7a38e7ea0ec3acaed6238
Dave Hall <dave@etianen.com>
2024-01-09 17:27:31 +0000

    wip

--- tests/test_pins.py
+++ tests/test_pins.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+from gpiozero.pins import Factory
+
 from radiopi.pins import PinFactory
 
 
 def test_pin_factory(pin_factory: PinFactory) -> None:
-    raise Exception
+    assert isinstance(pin_factory, Factory)
