f0184d77136779c8b8c44f19c8ea5df845641959
Dave Hall <dave@etianen.com>
2024-01-09 11:39:18 +0000

    wip

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 
 import logging
-from collections.abc import Generator
+from collections.abc import Generator, Sequence
 
 import pytest
 from gpiozero.pins.mock import MockFactory
 
 from radiopi.radio import Radio
 from radiopi.run import run_mock
+from radiopi.stations import Station, load_stations
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -15,13 +16,18 @@ def logging_config() -> None:
     logging.basicConfig(format="[%(levelname)s] %(message)s", level=logging.DEBUG)
 
 
+@pytest.fixture(scope="session")
+def stations() -> Sequence[Station]:
+    return load_stations()
+
+
 @pytest.fixture()
-def radio() -> Generator[Radio, None, None]:
+def radio(stations: Sequence[Station]) -> Generator[Radio, None, None]:
     # Create radio.
     radio = Radio(
         pin_factory=MockFactory(),
         run=run_mock,
-        stations=(),
+        stations=stations,
     )
     # Run radio.
     with radio.running():
--- tests/test_stations.py
+++ tests/test_stations.py
@@ -1,10 +1,17 @@
 from __future__ import annotations
 
-from radiopi.stations import Station, load_stations
+from collections.abc import Sequence
+
+from radiopi.stations import Station
 
 
-def test_load_stations() -> None:
-    stations = load_stations()
+def test_load_stations(stations: Sequence[Station]) -> None:
+    # Assert we load at least one station.
     assert stations
     for station in stations:
         assert isinstance(station, Station)
+
+
+def test_load_stations_unique(stations: Sequence[Station]) -> None:
+    # Assert we load a unique set of stations.
+    assert len(stations) == len(set(stations))
