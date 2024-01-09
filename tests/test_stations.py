364f3b2d33bc3b885a35951431ad44b9317efd1a
Dave Hall <dave@etianen.com>
2024-01-09 11:39:26 +0000

    wip

--- tests/test_stations.py
+++ tests/test_stations.py
@@ -6,12 +6,12 @@ from radiopi.stations import Station
 
 
 def test_load_stations(stations: Sequence[Station]) -> None:
-    # Assert we load at least one station.
+    # Ensure we load at least one station.
     assert stations
     for station in stations:
         assert isinstance(station, Station)
 
 
 def test_load_stations_unique(stations: Sequence[Station]) -> None:
-    # Assert we load a unique set of stations.
+    # Ensure we load a unique set of stations.
     assert len(stations) == len(set(stations))
