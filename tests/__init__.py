33f5c46ef75f051ef9c94e5d9714de84388f9053
Dave Hall <dave@etianen.com>
2024-01-13 22:29:42 +0000

    wip

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import logging
 from collections.abc import Generator
 from contextlib import contextmanager
-from itertools import chain
 from queue import Empty, SimpleQueue
 
 from radiopi import running as running_
@@ -36,20 +35,17 @@ class AwaitLog(logging.Handler):
     def assert_led_value(self, name: str, value: float) -> None:
         self(f"[DEBUG] LED: {name}: Value: {value}")
 
-    def __call__(self, *messages: str | tuple[str]) -> None:
-        # Normalize message groups.
-        message_groups: list[set[str]] = [set((m,) if isinstance(m, str) else m) for m in messages]
+    def __call__(self, *messages: str) -> None:
+        messages = set(messages)
         # Wait for all message groups to be satisfied.
-        while message_groups:
+        while messages:
             # Wait for a log record.
             try:
                 record = self.records.get(timeout=0.1)
             except Empty:
-                message_groups_str = "\n".join(chain.from_iterable(message_groups))
+                message_groups_str = "\n".join(messages)
                 raise AssertionError(f"Did not log:\n\n{message_groups_str}")
             # Check the log record.
             record_str = f"[{logging.getLevelName(record.levelno)}] {record.getMessage()}"
-            if record_str in message_groups[0]:
-                message_groups[0].remove(record_str)
-                if not message_groups[0]:
-                    message_groups.pop(0)
+            if record_str in messages:
+                messages.remove(record_str)
