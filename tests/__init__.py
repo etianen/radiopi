95921b9a5e554e61a3384e99f10dd94328236a67
Dave Hall <dave@etianen.com>
2024-01-13 22:37:04 +0000

    wip

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import dataclasses
 import logging
 from collections.abc import Generator
 from contextlib import contextmanager
@@ -29,23 +30,34 @@ class AwaitLog(logging.Handler):
     def emit(self, record: logging.LogRecord) -> None:
         self.records.put(record)
 
-    def assert_runner_called(self, args: Args) -> None:
-        self(f"[INFO] Runner: {' '.join(args)}")
-
-    def assert_led_value(self, name: str, value: float) -> None:
-        self(f"[DEBUG] LED: {name}: Value: {value}")
-
-    def __call__(self, *messages: str) -> None:
-        messages = set(messages)
+    def __call__(self, *expected_logs: ExpectedLog) -> None:
+        expected_logs = set(expected_logs)
         # Wait for all message groups to be satisfied.
-        while messages:
+        while expected_logs:
             # Wait for a log record.
             try:
                 record = self.records.get(timeout=0.1)
             except Empty:
-                message_groups_str = "\n".join(messages)
+                message_groups_str = "\n".join(map(str, expected_logs))
                 raise AssertionError(f"Did not log:\n\n{message_groups_str}")
             # Check the log record.
-            record_str = f"[{logging.getLevelName(record.levelno)}] {record.getMessage()}"
-            if record_str in messages:
-                messages.remove(record_str)
+            record_log = ExpectedLog(level=record.levelno, message=record.getMessage())
+            if record_log in expected_logs:
+                expected_logs.remove(record_log)
+
+
+@dataclasses.dataclass(frozen=True)
+class ExpectedLog:
+    level: int
+    message: str
+
+    @classmethod
+    def runner_call(cls, args: Args) -> ExpectedLog:
+        return ExpectedLog(level=logging.INFO, message=f"Runner: {' '.join(args)}")
+
+    @classmethod
+    def led_value(cls, name: str, value: float) -> ExpectedLog:
+        return ExpectedLog(level=logging.DEBUG, message=f"LED: {name}: Value: {value}")
+
+    def __str__(self) -> str:
+        return f"[{logging.getLevelName(self.level)}] {self.message}"
