a43f542b1667731571911d92a3462e99a1daa2f6
Dave Hall <dave@etianen.com>
2024-01-23 01:50:46 +0000

    Integrated `logot` (#9)

--- tests/test_radio.py
+++ tests/test_radio.py
@@ -1,27 +1,26 @@
 from __future__ import annotations
 
-import pytest
+from logot import Logot
 
-from tests import AwaitLog, ExpectedLog, running
+from tests import logged, running
 
 
-def test_running_pause_on_stop(await_log: AwaitLog) -> None:
+def test_running_pause_on_stop(logot: Logot) -> None:
     with running() as radio:
         # On start, the radio will boot and tune.
-        await_log(ExpectedLog.radio_boot())
-        await_log(ExpectedLog.radio_tune(radio.state.stations[0]))
+        logot.wait_for(logged.radio_boot())
+        logot.wait_for(logged.radio_tune(radio.state.stations[0]))
     # On stop, the radio automatically pauses.
-    await_log(ExpectedLog.radio_pause())
+    logot.wait_for(logged.radio_pause())
 
 
-def test_running_pause_on_stop_already_paused(await_log: AwaitLog) -> None:
+def test_running_pause_on_stop_already_paused(logot: Logot) -> None:
     with running() as radio:
         # On start, the radio will boot and tune.
-        await_log(ExpectedLog.radio_boot())
-        await_log(ExpectedLog.radio_tune(radio.state.stations[0]))
+        logot.wait_for(logged.radio_boot())
+        logot.wait_for(logged.radio_tune(radio.state.stations[0]))
         # Pause the radio.
         radio.pause()
-        await_log(ExpectedLog.radio_pause())
+        logot.wait_for(logged.radio_pause())
     # On stop, since the radio is already paused, nothing needs to be done.
-    with pytest.raises(AssertionError):
-        await_log(ExpectedLog.radio_pause())
+    logot.assert_not_logged(logged.radio_pause())
--- tests/test_ux.py
+++ tests/test_ux.py
@@ -3,82 +3,83 @@ from __future__ import annotations
 from collections.abc import Generator
 
 import pytest
+from logot import Logot
 
 from radiopi.radio import Radio
-from tests import AwaitLog, ExpectedLog, running
+from tests import logged, running
 
 
 @pytest.fixture()
-def radio(await_log: AwaitLog) -> Generator[Radio, None, None]:
+def radio(logot: Logot) -> Generator[Radio, None, None]:
     with running() as radio:
         # Wait for the radio to boot and tune.
-        await_log(ExpectedLog.ux_tune(radio.state.stations[0]))
+        logot.wait_for(logged.ux_tune(radio.state.stations[0]))
         # All done!
         yield radio
 
 
-def test_pause(radio: Radio, await_log: AwaitLog) -> None:
+def test_pause(radio: Radio, logot: Logot) -> None:
     radio.pause()
-    await_log(ExpectedLog.ux_pause())
+    logot.wait_for(logged.ux_pause())
 
 
-def test_toggle_play(radio: Radio, await_log: AwaitLog) -> None:
+def test_toggle_play(radio: Radio, logot: Logot) -> None:
     # This will pause the radio.
     radio.toggle_play()
-    await_log(ExpectedLog.ux_pause())
+    logot.wait_for(logged.ux_pause())
     # This will start the radio playing.
     radio.toggle_play()
-    await_log(ExpectedLog.ux_tune(radio.state.stations[0]))
+    logot.wait_for(logged.ux_tune(radio.state.stations[0]))
 
 
-def test_pause_pause_play(radio: Radio, await_log: AwaitLog) -> None:
+def test_pause_pause_play(radio: Radio, logot: Logot) -> None:
     radio.pause()
-    await_log(ExpectedLog.ux_pause())
+    logot.wait_for(logged.ux_pause())
     # This second pause does nothing, since we're already paused.
     radio.pause()
     # This play results in an action.
     radio.play()
-    await_log(ExpectedLog.ux_tune(radio.state.stations[0]))
+    logot.wait_for(logged.ux_tune(radio.state.stations[0]))
 
 
-def test_play_play_pause(radio: Radio, await_log: AwaitLog) -> None:
+def test_play_play_pause(radio: Radio, logot: Logot) -> None:
     # These plays do nothing, since we're already playing.
     radio.play()
     radio.play()
     # This pause results in an action.
     radio.pause()
-    await_log(ExpectedLog.ux_pause())
+    logot.wait_for(logged.ux_pause())
 
 
-def test_retune(radio: Radio, await_log: AwaitLog) -> None:
+def test_retune(radio: Radio, logot: Logot) -> None:
     radio.retune(3)
     # We're already booted, so we just tune.
-    await_log(ExpectedLog.ux_retune(radio.state.stations[3]))
+    logot.wait_for(logged.ux_retune(radio.state.stations[3]))
 
 
-def test_next_station(radio: Radio, await_log: AwaitLog) -> None:
+def test_next_station(radio: Radio, logot: Logot) -> None:
     radio.next_station()
     # We're already booted, so we just tune.
-    await_log(ExpectedLog.ux_next_station(radio.state.stations[1]))
+    logot.wait_for(logged.ux_next_station(radio.state.stations[1]))
 
 
-def test_prev_station(radio: Radio, await_log: AwaitLog) -> None:
+def test_prev_station(radio: Radio, logot: Logot) -> None:
     radio.prev_station()
     # We're already booted, so we just tune.
-    await_log(ExpectedLog.ux_prev_station(radio.state.stations[-1]))
+    logot.wait_for(logged.ux_prev_station(radio.state.stations[-1]))
 
 
-def test_pause_next_station(radio: Radio, await_log: AwaitLog) -> None:
+def test_pause_next_station(radio: Radio, logot: Logot) -> None:
     radio.pause()
-    await_log(ExpectedLog.ux_pause())
+    logot.wait_for(logged.ux_pause())
     # Since we're paused, this will first boot, then tune.
     radio.next_station()
-    await_log(ExpectedLog.ux_tune(radio.state.stations[1]))
+    logot.wait_for(logged.ux_tune(radio.state.stations[1]))
 
 
-def test_pause_prev_station(radio: Radio, await_log: AwaitLog) -> None:
+def test_pause_prev_station(radio: Radio, logot: Logot) -> None:
     radio.pause()
-    await_log(ExpectedLog.ux_pause())
+    logot.wait_for(logged.ux_pause())
     # Since we're paused, this will first boot, then tune.
     radio.prev_station()
-    await_log(ExpectedLog.ux_tune(radio.state.stations[-1]))
+    logot.wait_for(logged.ux_tune(radio.state.stations[-1]))
